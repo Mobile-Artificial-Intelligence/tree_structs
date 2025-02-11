a03e1e81c8693e38d0f00ab7b21bb3542e058ea3
danemadsen <dane_madsen@hotmail.com>
2025-02-11 16:18:54 +1000

    tests

--- test/trees_test.dart
+++ test/trees_test.dart
@@ -3,118 +3,156 @@ import 'package:trees/trees.dart';
 
 void main() {
   group('GeneralTreeNode', () {
-    test('addChild should correctly add child nodes', () {
-      final root = GeneralTreeNode('root');
-      root.addChild('child1');
-      root.addChild('child2');
-      root.addChild('child3');
+    test('constructor sets data and parent correctly', () {
+      final parent = GeneralTreeNode<int>(1);
+      final child = GeneralTreeNode<int>(2, parent);
 
-      expect(root.child!.data, 'child1');
-      expect(root.child!.nextSibling!.data, 'child2');
-      expect(root.child!.nextSibling!.nextSibling!.data, 'child3');
+      expect(child.data, 2);
+      expect(child.parent, parent);
+      expect(parent.children.isEmpty, true);
     });
 
-    test('removeChild should remove the correct child', () {
-      final root = GeneralTreeNode('root');
-      root.addChild('child1');
-      root.addChild('child2');
-      root.addChild('child3');
+    test('addChild adds a new child node', () {
+      final root = GeneralTreeNode<int>(1);
 
-      root.removeChild('child2');
+      root.addChild(2);
+      root.addChild(3);
 
-      expect(root.child!.data, 'child1');
-      expect(root.child!.nextSibling!.data, 'child3');
-      expect(root.child!.nextSibling!.nextSibling, isNull);
+      expect(root.children.length, 2);
+      expect(root.children[0].data, 2);
+      expect(root.children[1].data, 3);
     });
 
-    test('removeChild throws StateError if child not found', () {
-      final root = GeneralTreeNode('root');
-      root.addChild('child1');
+    test('removeChild removes a child by data', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
+      root.addChild(3);
 
-      expect(() => root.removeChild('child2'), throwsStateError);
+      root.removeChild(2);
+
+      expect(root.children.length, 1);
+      expect(root.children[0].data, 3);
     });
 
-    test('switchNextSibling updates the parents child pointer', () {
-      final root = GeneralTreeNode('root');
-      root.addChild('child1');
-      root.addChild('child2');
+    test('removeChild throws when child is not found', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
 
-      final child1 = root.child;
-      child1!.switchNextSibling();
-
-      expect(root.child!.data, 'child2');
+      expect(() => root.removeChild(3), throwsStateError);
     });
 
-    test('switchLastSibling updates the parents child pointer', () {
-      final root = GeneralTreeNode('root');
-      root.addChild('child1');
-      root.addChild('child2');
+    test('removeChildNode removes a specific child node', () {
+      final root = GeneralTreeNode<int>(1);
+      final child = GeneralTreeNode<int>(2, root);
+      root.children.add(child);
 
-      final child2 = root.child!.nextSibling;
-      child2!.switchLastSibling();
+      root.removeChildNode(child);
 
-      expect(root.child!.data, 'child1');
+      expect(root.children.isEmpty, true);
     });
 
-    test('switchNextSibling throws StateError if there is no next sibling', () {
-      final root = GeneralTreeNode('root');
-      root.addChild('child1');
+    test('siblings returns all siblings of a node', () {
+      final root = GeneralTreeNode<int>(1);
+      final child1 = GeneralTreeNode<int>(2, root);
+      final child2 = GeneralTreeNode<int>(3, root);
+      root.children.addAll([child1, child2]);
 
-      expect(() => root.child!.switchNextSibling(), throwsStateError);
+      expect(child1.siblings.length, 1);
+      expect(child1.siblings[0].data, 3);
     });
 
-    test('switchNextChild updates child pointer of a node', () {
-      final root = GeneralTreeNode('root');
-      root.addChild('child1');
-      root.child!.addChild('grandchild1');
-      root.child!.addChild('grandchild2');
+    test('chain returns the correct chain of nodes', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
+      root.children[0].addChild(3);
 
-      root.child!.switchNextChild();
-      expect(root.child!.child!.data, 'grandchild2');
+      final chain = root.chain;
+      expect(chain.length, 3);
+      expect(chain.map((node) => node.data), [1, 2, 3]);
     });
 
-    test('switchLastChild updates child pointer of a node', () {
-      final root = GeneralTreeNode('root');
-      root.addChild('child1');
-      root.child!.addChild('grandchild1');
-      root.child!.addChild('grandchild2');
+    test('bfs finds a node based on a condition', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
+      root.addChild(3);
+      root.children[0].addChild(4);
 
-      root.child!.switchNextChild();
-      expect(root.child!.child!.data, 'grandchild2');
-
-      root.child!.switchLastChild();
-      expect(root.child!.child!.data, 'grandchild1');
+      final result = root.bfs((data) => data == 4);
+      expect(result, 4);
     });
 
-    test('removeChild handles the case when the first child is removed', () {
-      final root = GeneralTreeNode('root');
-      root.addChild('child1');
-      root.addChild('child2');
+    test('dfs finds a node based on a condition', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
+      root.addChild(3);
+      root.children[0].addChild(4);
 
-      root.removeChild('child1');
-
-      expect(root.child!.data, 'child2');
+      final result = root.dfs((data) => data == 4);
+      expect(result, 4);
     });
 
-    test('removeChild handles removing all children', () {
-      final root = GeneralTreeNode('root');
-      root.addChild('child1');
+    test('nextChild increments the current child index', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
+      root.addChild(3);
 
-      root.removeChild('child1');
+      root.nextChild();
 
-      expect(root.child, isNull);
+      expect(root.currentChild, root.children[1]);
     });
 
-    test('complex tree structure works correctly', () {
-      final root = GeneralTreeNode('root');
-      root.addChild('child1');
-      root.addChild('child2');
-      root.child!.addChild('grandchild1');
-      root.child!.addChild('grandchild2');
+    test('previousChild decrements the current child index', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
+      root.addChild(3);
+      root.nextChild();
+      root.previousChild();
 
-      expect(root.child!.data, 'child1');
-      expect(root.child!.child!.data, 'grandchild1');
-      expect(root.child!.child!.nextSibling!.data, 'grandchild2');
+      expect(root.currentChild, root.children[0]);
+    });
+
+    test('childrenData returns the correct list of data', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
+      root.addChild(3);
+
+      expect(root.childrenData, [2, 3]);
+    });
+
+    test('siblingsData returns the correct list of sibling data', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
+      root.addChild(3);
+
+      expect(root.children[0].siblingsData, [3]);
+    });
+
+    test('chainData returns the correct list of chain data', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
+      root.children[0].addChild(3);
+
+      expect(root.chainData, [1, 2, 3]);
+    });
+
+    test('bfsNode returns the correct node', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
+      root.addChild(3);
+      root.children[0].addChild(4);
+
+      final result = root.bfsNode((node) => node.data == 4);
+      expect(result?.data, 4);
+    });
+
+    test('dfsNode returns the correct node', () {
+      final root = GeneralTreeNode<int>(1);
+      root.addChild(2);
+      root.addChild(3);
+      root.children[0].addChild(4);
+
+      final result = root.dfsNode((node) => node.data == 4);
+      expect(result?.data, 4);
     });
   });
 }
